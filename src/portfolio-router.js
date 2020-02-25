08ad089178931a89e2b429dafe14a618012edc86
amosguata <amosguata@gmail.com>
2020-02-26 00:06:33 +0200

    added google analytics

--- src/portfolio-router.js
+++ src/portfolio-router.js
@@ -4,10 +4,23 @@ import AdditionalInfo from "./Components/additional-info/additional-info";
 import {BrowserRouter as Router} from "react-router-dom";
 import {Route} from 'react-router-dom'
 import Header from "./Components/header/header";
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+
+const trackingId = "UA-159066855-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 function PortfolioRouter() {
 
-    return (<Router>
+    return (<Router history={history}>
                 <Header> </Header>
                 <div>
                     <Route exact path="/" component={App}/>
